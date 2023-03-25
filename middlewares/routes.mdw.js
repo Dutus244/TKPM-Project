61a92bb3176f37983d32fa4cbd05ac6ab4ed269f
anhthu872 <88284745+annthu872@users.noreply.github.com>
2023-03-26 02:52:12 +0700

    add: user-topic-list

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -1,10 +1,11 @@
 import authWithRequiredPermission from './auth.mdw.js';
 import { getVisiblePage } from "../utils/helper.js";
-
+import userRoute from '../routes/user.route.js'
 export default function (app) {
   app.get('/', async (req, res) => {
     res.render('vwGuest/home', {
       
     })
   })
+  app.use('/user',userRoute);
 }
