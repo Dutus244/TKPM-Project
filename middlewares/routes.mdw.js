4728fa7ab922ce930b34800460e47e36a8125ce4
anhthu872 <88284745+annthu872@users.noreply.github.com>
2023-03-26 23:54:25 +0700

    update: user-topics-list-with-readed

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -1,11 +1,11 @@
 import authWithRequiredPermission from './auth.mdw.js';
 import { getVisiblePage } from "../utils/helper.js";
-import userRoute from '../routes/user.route.js'
+import userRouter from '../routes/user.route.js'
 export default function (app) {
   app.get('/', async (req, res) => {
     res.render('vwGuest/home', {
       
     })
   })
-  app.use('/user',userRoute);
+  app.use('/user', userRouter)
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,8 +3,10 @@ import UserService from '../services/user.service.js'
 
 const router = express.Router();
 router.get('/topic', async function (req, res) {
-    const topiclist = await UserService.findAllTopic();
+    const temp = await UserService.findAllTopicStudy('c2229cc2-cbe1-11ed-b9d3-002248eb7c8a');
+    const topiclist = temp[0]
     console.log(topiclist)
+
     res.render('vwUser/topic', {
         topic: topiclist
     });
