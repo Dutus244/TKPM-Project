4f3b47917c23d3a4c5000251b0d36530a9ac1a29
congchuanhuy19 <81373090+ntny19@users.noreply.github.com>
2023-03-29 00:33:43 +0700

    build: add word in topic

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -15,9 +15,87 @@ export const config = {
     }
 }
 
-router.get('/', async function (req, res) {
+router.get('/detail/:id', async function (req, res) {
+    const topicid = req.params.id
+    const topic = await adminServices.getTopicDetail(topicid)
+    const word = await adminServices.countWords(topicid)
+    const test = await adminServices.getTest(topicid)
+    console.log(test)
     res.render('vwAdmin/topicdetail', {
-        
+        topic: topic,
+        num: word,
+        test: test
+    })
+})
+
+router.get('/addword/:id', async function (req, res){
+    const topicid = req.params.id
+    res.render('vwAdmin/addword',{
+        topicid: topicid,
+    })
+})
+
+router.post('/addword/:id', async function (req, res){
+    const id = v4()
+    const qid = v4()
+
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, './public/img/flashcard')
+        },
+        filename: function (req, file, cb) {
+            // const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+            cb(null, id + '.png')
+        }
+    })
+
+    const upload = multer({ storage: storage })
+    upload.array('fuMain', 1)(req, res, async function (err){
+
+        const topicid = req.params.id
+        const imagelink = '/public/img/flashcard' + id + '.png'
+
+        const wordname = req.body.wordname
+        const wordtype = req.body.wordtype
+        const wordmeaning = req.body.wordmeaning
+        const wordpronounce = req.body.wordpronounce
+        const wordexample = req.body.wordexample
+
+        const word={
+            topicid: topicid,
+            wordid: id,
+            wordname: wordname,
+            wordtype: wordtype,
+            wordmeaning: wordmeaning,
+            wordpronounce: wordpronounce,
+            wordexample: wordexample,
+            wordavatar: imagelink
+        }
+
+        const question = req.body.question
+        const optionA = req.body.optionA
+        const optionB = req.body.optionB
+        const optionC = req.body.optionC
+        const optionD = req.body.optionD
+
+        const test={
+            questionid: qid,
+            question: question,
+            optiona: optionA,
+            optionb: optionB,
+            optionc: optionC,
+            optiond: optionD
+        }
+
+        await adminServices.addWord(word)
+        await adminServices.addQuestion(test)
+        if (err instanceof multer.MulterError) {
+            // A Multer error occurred when uploading.
+            console.error(err);
+        } else if (err) {
+            // An unknown error occurred when uploading.
+            console.error(err);
+        }
     })
 })
 
@@ -46,8 +124,6 @@ router.post('/addtopic', async function (req, res){
         
         const topicname = req.body.topicname
         const imagelink = '/public/img/' + id + '.png'
-
-        const timestamp = new Date()
 
         const topic={
             topicid: id,
--- services/admin.service.js
+++ services/admin.service.js
@@ -1,7 +1,46 @@
 import db from '../utils/db.js';
 
 export default{
-    add(entity) {
-        return db('topics').insert(entity);
+    async add(entity) {
+        return await db('topics').insert(entity);
+    },
+
+    async getTopicDetail(id){
+        const topic = await db
+            .select('topics.topicid','topicname', 'topicavatar', 'wordid', 'wordname')
+            .from('topics')
+            .join('words')
+            .where('topics.topicid',id);
+        return topic;
+    },
+
+    async getTest(id){
+        const test = await db
+            .select('questionid')
+            .from('multiplechoicequestions')
+            .join('words')
+            .join('topics')
+            .where('topics.topicid',id);
+        if (test.length === 0) {
+            return null;
+        }
+            return test;
+    },
+
+    async countWords(id){
+        const list = await db
+            .count({amount: 'wordid'})
+            .from('words')
+            .join('topics')
+            .where('topics.topicid', id);
+        return list[0].amount
+    },
+
+    async addWord(entity){
+        return await db('words').insert(entity);
+    },
+
+    async addQuestion(entity){
+        return await db('multiplechoicequestions').insert(entity);
     },
 }
