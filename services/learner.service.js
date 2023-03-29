e5ec4a23053752ffdb8816dd201d902dc37bfbc9
Fox24 <73237980+duy2404@users.noreply.github.com>
2023-03-29 11:26:43 +0700

    Feature/nd user topic test

--- services/learner.service.js
+++ services/learner.service.js
@@ -7,5 +7,20 @@ export default {
       .where('topicid', topicId)
       
     return list
-  }
+  },
+  async findAllQuestionsTopic(id) {
+    const list = await db
+        .select('questionid','question','optiona','optionb','optionc','optiond','answer','picture','words.wordname')
+        .from('multiplechoicequestions')
+        .join('words','multiplechoicequestions.answer','words.wordid')
+        .join('topics','words.topicid','topics.topicid')
+        .where('topics.topicid', id);
+    return list;
+  },
+  async addTestHistory(entity) {
+    return db('testhistory').insert(entity);
+  },
+  async addTestHistoryDetail(entity) {
+    return db('testhistorydetail').insert(entity);
+  },
 }
