d26758a18ae73fda483223cf2450d93b479125dc
Samuel <20127610@student.hcmus.edu.vn>
2023-04-09 21:36:09 +0700

    register backend

--- routes/account.route.js
+++ routes/account.route.js
@@ -22,7 +22,34 @@ router.get('/register', async function(req, res) {
 })
 
 router.post('/register', async function(req, res) {
+  const salt = bcrypt.genSaltSync(10)
+  const hash = bcrypt.hashSync(req.body.password, salt)
+  const id = v4()
+  const timestamp = new Date()
 
+  const user = {
+    userid: id,
+    username: req.body.username,
+    pass: hash,
+    typeaccount: 0,
+    createtime: timestamp,
+    updatetime: timestamp,
+    lockaccount: false,
+    isdelete: false,
+  }
+
+  await accountService.addUser(user)
+  res.render('vwAccount/register', {
+    success: true,
+  })
+})
+
+router.get('/account/is-available', async function(req, res) {
+  const username = req.query.username
+  const user = await accountService.findByUsername(username)
+  if (user === null)
+    return res.json(true)
+  return res.json(false)
 })
 
 export default router
--- services/account.service.js
+++ services/account.service.js
@@ -1,5 +1,14 @@
 import db from '../utils/db.js';
 
 export default {
-  
+  async findByUsername(username) {
+    const list = await db('users').where('username', username)
+    if (list.length != 0)
+      return list[0]
+    return null
+  },
+
+  async addUser(entity) {
+    return await db('users').insert(entity)
+  }
 }
