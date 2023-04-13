90d2e81fed08f94efdb9f35a3bbb72f049d2dcab
anhthu872 <88284745+annthu872@users.noreply.github.com>
2023-04-13 15:22:55 +0700

    category ui update

--- routes/learner.route.js
+++ routes/learner.route.js
@@ -60,17 +60,106 @@ router.post('/topic/test/submit-answers', async function (req, res) {
   })
 });
 
-router.get('/topic', async function (req, res) {
+router.get('/topiclist', async function (req, res) {
   const temp = await learnerService.findAllTopicStudy('c2229cc2-cbe1-11ed-b9d3-002248eb7c8a');
   const topiclist = temp[0]
   res.render('vwLearner/topic', {
     topic: topiclist,
   });
 })
+router.get('/topiclist/:category_id', async function (req, res) {
+  const category_id = req.params.category_id;
+  const temp = await learnerService.findAllTopicStudy(category_id);
+  const topiclist = temp[0]
+  console.log(topiclist)
+  res.render('vwLearner/topic', {
+    topic: topiclist,
+  });
+})
 router.get('/category', async function (req, res) {
-  // const temp = await learnerService.findAllTopicStudy('c2229cc2-cbe1-11ed-b9d3-002248eb7c8a');
+  const total = await learnerService.countCategory();
+  const limit = 6;
+  const curPage = req.query.page || 1;
+  const offset = (curPage - 1) * limit;
+  const nPage = Math.ceil(total / limit);
+  const pageNumber = [];
+  for (let i = 1; i <= nPage; i++) {
+    pageNumber.push({
+      value: i,
+      isCurrent: i === +curPage,
+    });
+  }
+  const list = await learnerService.findCategoryByOffetWithLimit(
+      offset,
+      limit
+  );
+  console.log(list)
+  let prePage;
+  let nextPage;
+  if (1 === +curPage) {
+    prePage = 0;
+  } else {
+    prePage = +curPage - 1;
+  }
+  if (+nPage === +curPage) {
+    nextPage = 0;
+  } else if (+nPage === 0) {
+    nextPage = 0;
+  } else {
+    nextPage = +curPage + 1;
+  }
+  console.log(total)
   // const topiclist = temp[0]
+  // console.log(temp.count)
   res.render('vwLearner/category', {
+    category: list,
+    pageNumber: pageNumber,
+    empty: list.length === 0,
+    prePage: prePage,
+    nextPage: nextPage,
+  });
+})
+router.get('/category/:category_page', async function (req, res) {
+  const total = await learnerService.countCategory();
+  console.log(total)
+  const limit = 6;
+  const curPage = req.query.page || 1;
+  const offset = (curPage - 1) * limit;
+  const nPage = Math.ceil(total / limit);
+  const pageNumber = [];
+  for (let i = 1; i <= nPage; i++) {
+    pageNumber.push({
+      value: i,
+      isCurrent: i === +curPage,
+    });
+  }
+  const list = await learnerService.findCategoryByOffetWithLimit(
+      offset,
+      limit
+  );
+  console.log(list)
+  let prePage;
+  let nextPage;
+  if (1 === +curPage) {
+    prePage = 0;
+  } else {
+    prePage = +curPage - 1;
+  }
+  if (+nPage === +curPage) {
+    nextPage = 0;
+  } else if (+nPage === 0) {
+    nextPage = 0;
+  } else {
+    nextPage = +curPage + 1;
+  }
+  // const topiclist = temp[0]
+  // console.log(temp.count)
+  res.render('vwLearner/category', {
+    category: list,
+    pageNumber: pageNumber,
+    empty: list.length === 0,
+    prePage: prePage,
+    nextPage: nextPage,
   });
 })
 export default router;
