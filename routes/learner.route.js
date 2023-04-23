54031087cbb0829b2e6a584704540935f8d2e60a
Fox24 <73237980+duy2404@users.noreply.github.com>
2023-04-23 11:54:19 +0700

    ND: feature topic test history

--- routes/learner.route.js
+++ routes/learner.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import learnerService from "../services/learner.service.js";
 import bodyParser from 'body-parser';
-import { v4 } from 'uuid';
+import {v4} from 'uuid';
 import {PAGE_LIMIT} from './constants.js';
+import moment from 'moment';
+
 const router = express.Router();
 router.use(bodyParser.json());
-import moment from 'moment';
 
 router.get('/revision', async function(req, res) {
   let lessonsProgress = await learnerService.getLessonsProgress(res.locals.authUser.userid)
@@ -33,108 +34,109 @@ router.get('/revision', async function(req, res) {
   })
 })
 
-router.get("/topic/:id", async function(req, res) {
-  const id = req.params.id;
-  const wordlist = await learnerService.findAllTopicWord(id);
-  if (!wordlist.length) {
-    return res.status(404).render("404", {
-        layout: false,
-        active: {Learn: true }
+router.get("/topic/:id", async function (req, res) {
+    const id = req.params.id;
+    const wordlist = await learnerService.findAllTopicWord(id);
+    if (!wordlist.length) {
+        return res.status(404).render("404", {
+            layout: false,
+            active: {Learn: true}
+        });
+    }
+
+    res.render("vwLearner/topicLearn", {
+        words: JSON.stringify(wordlist),
+        firstWord: wordlist[0],
+        active: {Learn: true}
+
     });
-  }
-
-  res.render("vwLearner/topicLearn", {
-      words: JSON.stringify(wordlist),
-      firstWord: wordlist[0],
-      active: {Learn: true }
-
-  });
 });
 
-router.get("/topic/:id/finish", async function(req, res) {
-  const id = req.params.id
+router.get("/topic/:id/finish", async function (req, res) {
+    const id = req.params.id
 
-  res.render("vwLearner/topicLearnFinish", {
-      topicId: id,
-      active: {Learn: true }
-  })
+    res.render("vwLearner/topicLearnFinish", {
+        topicId: id,
+        active: {Learn: true}
+    })
 });
 
-router.post("/topic/:id/finish", async function(req, res) {
-  const topicid = req.params.id
-  const userid = res.locals.authUser.userid
-  const words = req.body.words
+router.post("/topic/:id/finish", async function (req, res) {
+    const topicid = req.params.id
+    const userid = res.locals.authUser.userid
+    const words = req.body.words
 
-  const hasLearned = await learnerService.hasLearnedTopic(userid, topicid)
-  if (hasLearned) {
-    return
-  }
+    const hasLearned = await learnerService.hasLearnedTopic(userid, topicid)
+    if (hasLearned) {
+        return
+    }
 
-  const finishWords = []
-  const timestamp = new Date()
-  const wordData = {
-    userid,
-    memorylevel: 1,
-    firsttime: timestamp,
-    updatetime: timestamp,
-    isstudy: true,
-  }
-  words.map(word_item => {
-    const wordid = word_item.wordid
-    finishWords.push({wordid, ...wordData})
-  })
-  await learnerService.addWordHistory(finishWords)
+    const finishWords = []
+    const timestamp = new Date()
+    const wordData = {
+        userid,
+        memorylevel: 1,
+        firsttime: timestamp,
+        updatetime: timestamp,
+        isstudy: true,
+    }
+    words.map(word_item => {
+        const wordid = word_item.wordid
+        finishWords.push({wordid, ...wordData})
+    })
+    await learnerService.addWordHistory(finishWords)
 
-  const topic = {
-      topicid,
-      userid,
-      createtime: timestamp,
-      active: {Learn: true }
-  }
-  await learnerService.addTopicHistory(topic)
+    const topic = {
+        topicid,
+        userid,
+        createtime: timestamp,
+        active: {Learn: true}
+    }
+    await learnerService.addTopicHistory(topic)
 })
 
 router.get('/topic/test/:id', async function (req, res) {
-  const topicid = req.params.id
-  const listQuestion = await learnerService.findAllQuestionsTopic(topicid)
-  res.render('vwLearner/topicTest', {
-      topicid: topicid,
-      question: listQuestion,
-      active: {Learn: true }
-  });
+    const topicid = req.params.id
+    const listQuestion = await learnerService.findAllQuestionsTopic(topicid)
+    res.render('vwLearner/topicTest', {
+        topicid: topicid,
+        question: listQuestion,
+        active: {Learn: true}
+    });
 });
 
 router.post('/topic/test/submit-answers', async function (req, res) {
-  const userAnswers = await req.body;
-  const {topicid} = userAnswers
-  const id = v4()
-  const testhistory = {
-      testid: id,
-      topicid: topicid,
-      userid: req.session.authUser.userid,
-      createtime: moment().format('YYYY-MM-DD HH:mm:ss'),
-      active: {Learn: true }
-  };
-  const data = await learnerService.addTestHistory(testhistory)
-  const testhistorydetails = userAnswers.answers.map(item => {
-    return {
-      testid: id,
-      questionid: item.questionID,
-      userchoose: item.userchoose,
-      optiona: item.optiona,
-      optionb: item.optionb,
-      optionc: item.optionc,
-      optiond: item.optiond
+    const userAnswers = await req.body;
+    const {topicid, totalQuestions, totalCorrect} = userAnswers
+    const id = v4()
+    const testhistory = {
+        testid: id,
+        topicid: topicid,
+        userid: req.session.authUser.userid,
+        createtime: moment().format('YYYY-MM-DD HH:mm:ss'),
+        totalcorrect: totalCorrect,
+        totalquestion: totalQuestions,
     };
-  });
+    const data = await learnerService.addTestHistory(testhistory)
+    const testhistorydetails = userAnswers.answers.map(item => {
+        return {
+            testid: id,
+            questionid: item.questionID,
+            userchoose: item.userchoose,
+            optiona: item.optiona,
+            optionb: item.optionb,
+            optionc: item.optionc,
+            optiond: item.optiond
+        };
+    });
 
-  const results = await Promise.all(testhistorydetails.map(testhistorydetail => {
-    return learnerService.addTestHistoryDetail(testhistorydetail);
-  }));
-  res.render("vwLearner/topicTestFinish", {
-      topicId: topicid,
-      active: {Learn: true }
-  })
+    const results = await Promise.all(testhistorydetails.map(testhistorydetail => {
+        return learnerService.addTestHistoryDetail(testhistorydetail);
+    }));
+    res.render("vwLearner/topicTestFinish", {
+        topicId: topicid,
+        active: {Learn: true}
+    })
 });
 
 router.get('/topiclist/:lesson_id', async function (req, res) {
@@ -145,10 +147,11 @@ router.get('/topiclist/:lesson_id', async function (req, res) {
     res.render('vwLearner/topic', {
         topic: topiclist,
         lesson,
-        active: {Learn: true }
+        active: {Learn: true}
     });
 })
-function setup_pages(curPage, nPage){
+
+function setup_pages(curPage, nPage) {
     let prePage = 0;
     let nextPage;
     if (1 === +curPage) {
@@ -163,12 +166,13 @@ function setup_pages(curPage, nPage){
     } else {
         nextPage = +curPage + 1;
     }
-    if (+nPage < +curPage){
+    if (+nPage < +curPage) {
         nextPage = 0
         prePage = 0
     }
-        return [prePage , nextPage]
+    return [prePage, nextPage]
 }
+
 router.get('/lesson', async function (req, res) {
     const total = await learnerService.countLesson();
     const limit = PAGE_LIMIT;
@@ -177,16 +181,16 @@ router.get('/lesson', async function (req, res) {
     const nPage = Math.ceil(total / limit);
     const pageNumber = [];
     for (let i = 1; i <= nPage; i++) {
-      pageNumber.push({
-        value: i,
-        isCurrent: i === +curPage,
-      });
+        pageNumber.push({
+            value: i,
+            isCurrent: i === +curPage,
+        });
     }
     const list = await learnerService.findLessonByOffetWithLimit(
         offset,
         limit
     );
-    let [prePage, nextPage] = setup_pages(curPage,nPage);
+    let [prePage, nextPage] = setup_pages(curPage, nPage);
 
     res.render('vwLearner/lesson', {
         lesson: list,
@@ -194,7 +198,7 @@ router.get('/lesson', async function (req, res) {
         empty: list.length === 0,
         prePage: prePage,
         nextPage: nextPage,
-        active: {Learn: true }
+        active: {Learn: true}
 
     });
 })
@@ -206,43 +210,76 @@ router.get('/lesson/:lesson_page', async function (req, res) {
     const nPage = Math.ceil(total / limit);
     const pageNumber = [];
     for (let i = 1; i <= nPage; i++) {
-      pageNumber.push({
-        value: i,
-        isCurrent: i === +curPage,
-      });
+        pageNumber.push({
+            value: i,
+            isCurrent: i === +curPage,
+        });
     }
     const list = await learnerService.findLessonByOffetWithLimit(
         offset,
         limit
     );
-    let [prePage, nextPage] = setup_pages(curPage,nPage);
+    let [prePage, nextPage] = setup_pages(curPage, nPage);
     res.render('vwLearner/lesson', {
         lesson: list,
         pageNumber: pageNumber,
         empty: list.length === 0,
         prePage: prePage,
         nextPage: nextPage,
-        active: {Learn: true }
+        active: {Learn: true}
 
     });
 })
 
 router.get('/dailytest', async function (req, res) {
-  const list = await learnerService.findAllQuestionDailyTest()
-  res.render('vwLearner/dailyTest', {
-      question: list,
-      active: {Review: true }
-  });
+    const userID = req.session.authUser.userid
+    const list = await learnerService.findAllQuestionDailyTest(userID)
+    res.render('vwLearner/dailyTest', {
+        empty: list.length === 0,
+        question: list,
+        active: {Review: true}
+    });
 })
 router.get('/resultdailytest', async function (req, res) {
-  const userID = req.session.authUser.userid
-  const {wordID, check} = req.query;
-  await learnerService.updateMemoryLevel(userID, wordID, check)
+    const userID = req.session.authUser.userid
+    const {wordID, check} = req.query;
+    await learnerService.updateMemoryLevel(userID, wordID, check)
 })
 router.get('/handbook', async function (req, res) {
     res.render('vwLearner/handbook', {
-        active: {Handbook: true }
+        active: {Handbook: true}
 
+    });
+})
+router.get('/topictesthistorylist', async function (req, res) {
+    const userID = req.session.authUser.userid
+    const list = await learnerService.getTestHistory(userID)
+    const listlesson = await learnerService.findLesson()
+    res.render('vwLearner/topicTestHistoryList', {
+        listlesson: listlesson,
+        empty: list.length === 0,
+        list: list,
+        active: {Handbook: true}
+    });
+})
+router.get('/getalltopichistory', async function (req, res) {
+    const userID = req.session.authUser.userid
+    const list = await learnerService.getTestHistory(userID)
+    res.send(list)
+})
+router.get('/gettopichistory', async function (req, res) {
+    const {lessonid} = req.query;
+    const userID = req.session.authUser.userid
+    const list = await learnerService.getTestHistoryByLesson(userID, lessonid)
+    res.send(list)
+})
+
+router.get('/topictesthistory/:test_id', async function (req, res) {
+    const {test_id} = req.params;
+    const list = await learnerService.getTestDetail(test_id)
+    res.render('vwLearner/topicTestHistoryDetail', {
+        list: list,
+        active: {Learn: true}
     });
 })
 export default router;
