09a8b9ef4cd1d1f3ff038c07f78a6ea2bdfff06c
congchuanhuy19 <81373090+ntny19@users.noreply.github.com>
2023-04-28 22:57:10 +0700

    fix: put account requirements in constant

--- routes/constants.js
+++ routes/constants.js
@@ -1,1 +1,8 @@
 export const PAGE_LIMIT = 12
+export const account = [
+    { memlvl: 1, numFinished: 1, accountlvl: 'Seed', linkimage:"/public/img/background/seed.png" },
+    { memlvl: 3, numFinished: 10, accountlvl: 'Germ', linkimage:"/public/img/background/germ.png" },
+    { memlvl: 10, numFinished: 20, accountlvl: 'Bud', linkimage:"/public/img/background/bud.png" },
+    { memlvl: 40, numFinished: 30, accountlvl: 'Tree', linkimage:"/public/img/background/tree.png" },
+    { memlvl: 80, numFinished: 40, accountlvl: 'Flower', linkimage:"/public/img/background/flower.png" },
+]
